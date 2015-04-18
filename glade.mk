927987d40e1f445d11b375786e0a1e09ecebe199
rohan <purohit.rohan@gmail.com>
2015-04-18 11:10:30 +0200

    glade-ify

--- glade.mk
+++ glade.mk
@@ -1,12 +1,9 @@
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/glade/configs/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
 
-PRODUCT_NAME := cm_yuga
+PRODUCT_NAME := glade_yuga
 PRODUCT_DEVICE := yuga
